4430ef7395b1321f63390dc387f7d908c8d0ac6a
Roe Lez <lezzarrami@gmail.com>
2024-02-23 16:25:17 +0100

    Home + Sidebar

--- Admin Panel/src/Home.jsx
+++ Admin Panel/src/Home.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import {
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsFillBellFill,
+} from "react-icons/bs";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+} from "recharts";
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="main-container">
+      <div className="main-title">
+        <h3>Tableau de bord</h3>
+      </div>
+      <div className="main-cards">
+        <div className="card">
+          <div className="card-inner">
+            <h3>Produits</h3>
+            <BsFillArchiveFill className="card-icon" />
+          </div>
+          <h1>300</h1>
+        </div>
+        <div className="card">
+          <div className="card-inner">
+            <h3>Categories</h3>
+            <BsFillGrid3X3GapFill className="card-icon" />
+          </div>
+          <h1>23</h1>
+        </div>
 
-export default Home
+        <div className="card">
+          <div className="card-inner">
+            <h3>Clients</h3>
+            <BsPeopleFill className="card-icon" />
+          </div>
+          <h1>35</h1>
+        </div>
+        <div className="card">
+          <div className="card-inner">
+            <h3>Alerts</h3>
+            <BsFillBellFill className="card-icon" />
+          </div>
+          <h1>4</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- Admin Panel/src/Sidebar.jsx
+++ Admin Panel/src/Sidebar.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <aside id="sidebar">
+      <div className="sidebar-title">
+        <div className="sidebar-brand">
+          <BsCart3 className="icon" /> SHOP
+        </div>
+        <span className="icon close_icon">X</span>
+      </div>
+
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsGrid1X2Fill className="icon" /> Tableau de bord
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillArchiveFill className="icon" /> Produits
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillGrid3X3GapFill className="icon" /> Categories
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsPeopleFill className="icon" /> Clients
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsListCheck className="icon" /> Inventaire
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsMenuButtonWideFill className="icon" /> Raports
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsFillGearFill className="icon" /> Paramètres
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
